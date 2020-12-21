31af0cf536788e50437f06aa20026769713b9277
Yevhenii <evg.gordi@gmail.com>
2020-12-21 12:32:42 +0100

    Add project files.

---
+++ Models/ApplicationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Models
+{
+    public class ApplicationModel
+    {
+        public string Name { get; set; }
+        public string Desciption { get; set; }
+
+        public string LanguageTag { get; set; }
+
+        public List<string> Images { get; set; }
+
+    }
+}
